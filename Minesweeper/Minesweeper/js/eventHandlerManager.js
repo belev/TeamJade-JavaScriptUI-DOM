98abfc80e66c45789f94f4a751e21256bcfcee6f
Martin Belev <martinbelev93@gmail.com>
2014-06-11 18:25:43 -0700

    All removed functions from minesweeper.js are refactored and now used from the EventHandlerManager module in eventHandlerManager.js file.

---
+++ Minesweeper/Minesweeper/js/eventHandlerManager.js
@@ -0,0 +1,127 @@
+var EventHandlerManager = (function () {
+    function openCell(e) {
+        if (ms.isGameOver || ms.isGameWon) {
+            return;
+        }
+
+        e = e || window.event;
+        e.preventDefault();
+
+        var x = e.pageX - Game.canvas[0].offsetLeft,
+            y = e.pageY - Game.canvas[0].offsetTop,
+            colPos = Math.floor(x / ms.sprites.cell.w),
+            rowPos = Math.floor(y / ms.sprites.cell.h);
+
+        if (colPos >= 0 && colPos < ms.settings.cols &&
+            rowPos >= 0 && rowPos < ms.settings.rows) {
+
+            if (!Playfield.isFirstClicked) {
+                ms.playfield = Playfield.initialize(ms.settings.rows,
+                    ms.settings.cols,
+                    ms.settings.mines,
+                    rowPos, colPos);
+                Playfield.isFirstClicked = true;
+            }
+
+            if (!ms.playfield[rowPos][colPos].isFlagged &&
+                ms.playfield[rowPos][colPos].hasMine) {
+                ms.gameover();
+
+            } else if (!ms.playfield[rowPos][colPos].isFlagged) {
+                // click on the cell, if the cell is empty, open all the empty cells around it
+                clickCell(rowPos, colPos);
+            }
+
+            ms.drawPlayfield();
+        }
+    }
+
+    function putFlag(e) {
+        if (ms.isGameOver || ms.isGameWon) {
+            return;
+        }
+
+        e = e || window.event;
+        e.preventDefault();
+
+        var x = e.pageX - Game.canvas[0].offsetLeft,
+            y = e.pageY - Game.canvas[0].offsetTop,
+            colPos = Math.floor(x / ms.sprites.cell.w),
+            rowPos = Math.floor(y / ms.sprites.cell.h);
+
+        if (colPos >= 0 && colPos < ms.settings.cols &&
+            rowPos >= 0 && rowPos < ms.settings.rows) {
+
+            colPos = Math.floor(x / ms.sprites.cell.w);
+            rowPos = Math.floor(y / ms.sprites.cell.h);
+
+            if (!ms.playfield[rowPos][colPos].isRevealed) {
+                if (ms.playfield[rowPos][colPos].isFlagged) {
+                    unflagCell(rowPos, colPos);
+
+                } else if (!ms.playfield[rowPos][colPos].isFlagged &&
+                    ms.flagsLeft > 0) {
+                    flagCell(rowPos, colPos);
+                }
+            }
+
+            if (ms.unrevealedCount === ms.settings.mines && ms.flagsLeft === 0) {
+                ms.gameWon();
+            }
+
+            ms.drawPlayfield();
+        }
+    }
+
+// if clicked on empty cell traverse all neighbour empty cells and open them
+// if clicked on full cell open only the clicked cell
+    function clickCell(row, col) {
+        // check if the cell we want to reveal is in the play field
+        if (col < 0 || row < 0 ||
+            col >= ms.settings.cols || row >= ms.settings.rows) {
+            return;
+        }
+
+        if (ms.playfield[row][col].isRevealed) {
+            return;
+        }
+
+        setCellToBeRevealed(row, col);
+
+        if (ms.playfield[row][col].neighbourMinesCount === 0) {
+            for (var neighbourRow = row - 1; neighbourRow <= row + 1; neighbourRow++) {
+                for (var neighbourCol = col - 1; neighbourCol <= col + 1; neighbourCol++) {
+                    if (row != neighbourRow || col != neighbourCol) {
+                        clickCell(neighbourRow, neighbourCol);
+                    }
+                }
+            }
+        }
+    }
+
+    function setCellToBeRevealed(row, col) {
+        ms.playfield[row][col].isRevealed = true;
+        ms.playfield[row][col].frame = 1;
+        ms.unrevealedCount -= 1;
+
+        // if the cell is flagged, un-flag it
+        if (ms.playfield[row][col].isFlagged) {
+            unflagCell(row, col);
+        }
+    }
+
+    function flagCell(row, col) {
+        ms.playfield[row][col].isFlagged = true;
+        ms.flagsLeft -= 1;
+    }
+
+    function unflagCell(row, col) {
+        ms.playfield[row][col].isFlagged = false;
+        ms.flagsLeft += 1;
+    }
+
+    return {
+        openCell: openCell,
+        putFlag: putFlag
+    }
+}());
